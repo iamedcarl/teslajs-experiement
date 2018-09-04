5d501c51a1235f6599876a82c85b11002c9c4214
Edcarl Adraincem <iamedcarl@users.noreply.github.com>
2018-09-03 23:14:03 -0700

    Make request to get vehicle information and data

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 import tjs from 'teslajs';
 import settings from './settings/local.json';
 
-const username = settings.username;
-const password = settings.password;
+let options = {};
 
-tjs.login(username, password, function(err, result) {
+// const generateToken = () => {
+//
+// };
+
+tjs.login(settings.username, settings.password, (err , result) => {
   if (result.error) {
     console.log(JSON.stringify(result.error));
-    process.exit(1);
+    return false;
   }
 
   const token = JSON.stringify(result.authToken);
 
   if (token) {
     console.log("Login Succesful!");
+    options['authToken'] = result.authToken;
+
+    tjs.vehiclesAsync(options).done(function(vehicle) {
+      console.log("[Vehicle] ", vehicle);
+      options['vehicleID'] = vehicle.id;
+
+      tjs.vehicleDataAsync(options).done(function(vehicleData) {
+        console.log("[Vehicle Data] ", vehicleData);
+      });
+    });
   }
 });
